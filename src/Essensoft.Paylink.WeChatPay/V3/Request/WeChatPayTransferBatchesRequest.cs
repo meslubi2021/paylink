3f71d71e2f509df58bc59885a9ddb732dbfe445a
roc <roc@essensoft.cn>
2024-06-28 02:37:51 +0800

    [[WeChatPay.V3] 更新 发起商家转账API

--- src/Essensoft.Paylink.WeChatPay/V3/Request/WeChatPayTransferBatchesRequest.cs
+++ src/Essensoft.Paylink.WeChatPay/V3/Request/WeChatPayTransferBatchesRequest.cs
@@ -3,10 +3,10 @@
 namespace Essensoft.Paylink.WeChatPay.V3.Request;
 
 /// <summary>
-/// 发起商家转账API - 最新更新时间：2022.04.24
+/// 发起商家转账API - 最新更新时间：2024.03.22
 /// </summary>
 /// <remarks>
-/// 商户可以通过该接口同时向多个用户微信零钱进行转账操作。
+/// 发起商家转账接口。商户可以通过该接口同时向多个用户微信零钱进行转账操作。请求消息中应包含商家批次单号、转账名称、appid、转账总金额、转账总笔数、转账openid、收款用户姓名等信息。注意受理成功将返回批次单号，此时并不代表转账成功，请通过查单接口查询单据的付款状态
 /// </remarks>
 public class WeChatPayTransferBatchesRequest : IWeChatPayPrivacyPostRequest<WeChatPayTransferBatchesResponse>
 {
